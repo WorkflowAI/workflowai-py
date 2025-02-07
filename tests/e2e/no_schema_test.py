a84e9d34939e8aa70ecd9609e89c18c41203f539
Guillaume Aquilina <guillaume@workflowai.com>
2025-02-07 14:25:53 -0500

    test: fix e2e test in summary

--- tests/e2e/no_schema_test.py
+++ tests/e2e/no_schema_test.py
@@ -18,5 +18,13 @@ async def summarize(_: SummarizeTaskInput) -> SummarizeTaskOutput: ...
 
 
 async def test_summarize():
-    summarized = await summarize(SummarizeTaskInput(text="Hello, world!"))
+    summarized = await summarize(
+        SummarizeTaskInput(
+            text="""The first computer programmer was Ada Lovelace. She wrote the first algorithm
+intended to be processed by a machine in the 1840s. Her work was on Charles Babbage's
+proposed mechanical computer, the Analytical Engine. She is celebrated annually on Ada
+Lovelace Day, which promotes women in science and technology.""",
+        ),
+        use_cache="never",
+    )
     assert summarized.summary_points
