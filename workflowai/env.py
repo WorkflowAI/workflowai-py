530d2f6a35d3bece0dcae0349cd35d8496bd3f07
Guillaume Aquilina <guillaume@workflowai.com>
2025-02-16 16:10:04 -0500

    feat: smarter default app url

--- workflowai/env.py
+++ workflowai/env.py
@@ -2,10 +2,21 @@
 
 import os
 
-WORKFLOWAI_APP_URL = os.getenv("WORKFLOWAI_APP_URL", "workflowai.com")
-
 WORKFLOWAI_DEFAULT_MODEL = os.getenv("WORKFLOWAI_DEFAULT_MODEL", "gemini-1.5-pro-latest")
 
 WORKFLOWAI_API_URL = os.getenv("WORKFLOWAI_API_URL")
+
+
+def _default_api_url():
+    if not WORKFLOWAI_API_URL:
+        return "https://workflowai.com"
+    if WORKFLOWAI_API_URL.startswith("https://run."):
+        return "https://" + WORKFLOWAI_API_URL.removeprefix("https://run.")
+    if WORKFLOWAI_API_URL.startswith("https://api."):
+        return "https://" + WORKFLOWAI_API_URL.removeprefix("https://api.")
+    return "https://workflowai.com"
+
+
+WORKFLOWAI_APP_URL = os.getenv("WORKFLOWAI_APP_URL", _default_api_url())
 
 WORKFLOWAI_API_KEY = os.getenv("WORKFLOWAI_API_KEY", "")
