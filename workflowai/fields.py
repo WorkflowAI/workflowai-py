1ad1ebb556dce24b5df19ddc56f940d1d007f0bb
Guillaume Aquilina <guillaume@workflowai.com>
2025-02-24 16:25:58 -0500

    feat: add pdf object

--- workflowai/fields.py
+++ workflowai/fields.py
@@ -6,6 +6,7 @@ from workflowai.core.fields.html_string import HTMLString as HTMLString
 from workflowai.core.fields.http_url import HttpUrl as HttpUrl
 from workflowai.core.fields.image import Image as Image
 from workflowai.core.fields.local_date_time import DatetimeLocal as DatetimeLocal
+from workflowai.core.fields.pdf import PDF as PDF
 from workflowai.core.fields.price import Price as Price
 from workflowai.core.fields.surface_area import SurfaceArea as SurfaceArea
 from workflowai.core.fields.zone_info import TimezoneInfo as TimezoneInfo
